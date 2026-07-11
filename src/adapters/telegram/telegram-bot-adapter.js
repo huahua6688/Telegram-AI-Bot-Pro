7fa11870ebda09defa7cd5704fa96350aad00c80
huahua6688 <huahuayanyuan@gmail.com>
2026-07-11 19:01:49 +0800

    Describe hidden features in help

--- src/adapters/telegram/telegram-bot-adapter.js
+++ src/adapters/telegram/telegram-bot-adapter.js
@@ -1,5 +1,23 @@
-import { PrivacyTelegramAIBot } from '../../services/privacy-telegram-bot.js';
+import { HelpTelegramAIBot } from '../../services/help-telegram-bot.js';
 
-export function createTelegramBot({ config, db, aiClient, providerManager, toolRegistry, pluginManager, logger, accessControl }) {
-  return new PrivacyTelegramAIBot({ config, db, aiClient, providerManager, toolRegistry, pluginManager, logger, accessControl });
+export function createTelegramBot({
+  config,
+  db,
+  aiClient,
+  providerManager,
+  toolRegistry,
+  pluginManager,
+  logger,
+  accessControl
+}) {
+  return new HelpTelegramAIBot({
+    config,
+    db,
+    aiClient,
+    providerManager,
+    toolRegistry,
+    pluginManager,
+    logger,
+    accessControl
+  });
 }
